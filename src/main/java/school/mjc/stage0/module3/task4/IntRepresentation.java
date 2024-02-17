5364cdbf8c8e3822c885bf49cfe860d513129d7d
SobakaBekka <st69704@upce.cz>
2024-02-17 20:56:33 +0100

    Done the tasks

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char a = 99;
+        char b = 104;
+        char c = 97;
+        char d = 114;
+        System.out.println((char)a);
+        System.out.println((char)b);
+        System.out.println((char)c);
+        System.out.println((char)d);
+
     }
 }
